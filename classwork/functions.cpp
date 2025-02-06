0939fab9187ed03ed670e9e840901cbcf75e7682
Aubrey Charlson <alcharlson@mavs.coloradomesa.edu>
2025-02-06 09:43:42 -0700

    functions.cpp

---
+++ classwork/functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+string promptName()
+{
+    string uName;
+    cout << "Please enter a username: " <<endl;
+    cin >> uName;
+    cout << "Hello, " << uName << "!" <<endl;
+
+}
+double moreNums() 
+{
+        return 0.0;
+}
+
+float reNum() 
+{
+        return 42.5;
+}
+
+int main () 
+{
+        return 0;
+}
