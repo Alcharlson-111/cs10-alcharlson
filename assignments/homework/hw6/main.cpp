0e31eb4864d140b1768b3590ef23171665e46961
aubrey charlson <alcharlson@mavs.coloradomesa.edu>
2025-04-28 21:58:10 -0700

    Started l9, hw6

---
+++ assignments/homework/hw6/main.cpp
@@ -0,0 +1,53 @@
+/*
+Author: Aubrey Charlson
+Date: 4/28
+Class: CS111
+
+    Program: Divides series of integers between two variables based on highest remaining int. 
+
+    Algorithmn Steps: 
+    1. Input array size
+    2. Input array entries
+    3. Loop
+        a. Find highest entry += to current var
+        b. Remove entry from array. 
+    4.     */
+
+#include <iostream>
+#include <array> 
+#include <sstream>
+
+using namespace std;
+
+int getLength(); 
+string getPieces();
+void appendArray(int length, int []);
+
+
+int main(int argv, char* argc[])
+{
+    
+}
+
+int getLength()
+{
+    int length;
+    cin >> length;
+    return length;
+}
+
+void appendArray(int length, int arr1[])
+{
+    string pieces;
+    getline(cin, pieces);
+    int curPos0;
+    while ((curPos0 = pieces.find(" ", != pieces::n_pos)
+    
+
+}
+
+
+
+
+
+
